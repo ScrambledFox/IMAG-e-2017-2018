502e700cbd8c88d874196341c2c3da20490817c2
Joris Lodewijks <joris.lodewijks@hotmail.com>
2018-11-11 20:52:39 +0100

    End of Sunday

---
+++ IMAGe Prototype/Assets/Scripts/LogoInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoInput : MonoBehaviour {
+
+    SceneManager manager;
+
+    private void Awake () {
+        manager = transform.parent.GetComponent<SceneManager>();
+    }
+
+    private void Update () {
+
+        if (Input.touchCount > 0) {
+            manager.GoToScene(1);
+            Destroy(this);
+        }
+
+        if (Input.GetMouseButtonDown(0)) {
+            manager.GoToScene(1);
+            Destroy(this);
+        }
+
+    }
+}
